5bb83bd023b28b8530f1cb78a5bba5819712c3c0
kwonsoryeong <pjh9438@naver.com>
2023-07-11 10:16:14 +0900

    refactor: 프로젝트 이름 수정

--- ios/smalltoy/AppDelegate.mm
+++ ios/smalltoy/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"Toy";
+  self.moduleName = @"small_toy";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
--- ios/smalltoyTests/smalltoyTests.m
+++ ios/smalltoyTests/smalltoyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ToyTests : XCTestCase
+@interface smalltoyTests : XCTestCase
 
 @end
 
-@implementation ToyTests
+@implementation smalltoyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
